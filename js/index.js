52e65d8ba5f631c0deb72dcee06e1432b1e94e63
heicx <heicx_sudo@163.com>
2014-10-22 01:23:22 +0800

    add arrow style

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 window.onload = function(){
-	console.log("test");
+	var arrow_obj = document.getElementById("arrow").getElementsByTagName("span");
+	var arrow_l_obj = arrow_obj[0];
+	var arrow_r_obj = arrow_obj[1];
+
+	arrow_l_obj.onclick = function(){
+		console.log(123);
+	}
 };
